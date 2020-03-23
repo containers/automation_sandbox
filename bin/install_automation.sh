2862625a4fa182d444b08a7b48081022a47f9742
Chris Evich <cevich@redhat.com>
2020-03-23 12:33:39 -0400

    complete/fix testing for installer
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- bin/install_automation.sh
+++ bin/install_automation.sh
@@ -22,21 +22,29 @@ AUTOMATION_REPO_BRANCH=${AUTOMATION_REPO_BRANCH:-master}
 SCRIPT_FILENAME=$(basename $0)
 # The source version requested for installing
 AUTOMATION_VERSION="$1"
+# Set non-zero to enable
+DEBUG=${DEBUG:-0}
+# Save a bit of typin
+OOE="./common/bin/ooe.sh"
 # Sentinel value representing whatever version is present in the local repository
 MAGIC_LOCAL_VERSION='0.0.0'
 # Needed for unit-testing
 DEFAULT_INSTALL_PREFIX=/usr/local/share
 INSTALL_PREFIX="${INSTALL_PREFIX:-$DEFAULT_INSTALL_PREFIX}"
 # Used internally here and in unit-testing, do not change without a really, really good reason.
+_ARGS="$@"
 _MAGIC_JUJU=${_MAGIC_JUJU:-XXXXX}
 _DEFAULT_MAGIC_JUJU=d41d844b68a14ee7b9e6a6bb88385b4d
 
-msg() { echo "${1:-No Message given}" > /dev/stderr; }
+msg() { echo -e "${1:-No Message given}" > /dev/stderr; }
+
+dbg() { if ((DEBUG)); then msg "\n# $1"; fi }
 
 # Represents specific installer behavior, should that ever need to change
 d41d844b68a14ee7b9e6a6bb88385b4d() {
     TEMPDIR=$(realpath "$(dirname $0)/../")
     trap "rm -rf $TEMPDIR" EXIT
+    dbg "Will clean up \$TEMPDIR upon script exit"
 
     if [[ "$AUTOMATION_VERSION" == "$MAGIC_LOCAL_VERSION" ]] || [[ "$AUTOMATION_VERSION" == "latest" ]]; then
         msg "BUG: Actual installer requires actual version number, not '$AUTOMATION_VERSION'"
@@ -44,43 +52,59 @@ d41d844b68a14ee7b9e6a6bb88385b4d() {
     fi
 
     local actual_inst_path="$INSTALL_PREFIX/automation"
-    msg "Installing common scripts/libraries version '$AUTOMATION_VERSION' into '$actual_inst_path'"
-
+    # Name Hack: if/when installed globally, should work for both Fedora and Debian-based
+    spp="etc/profile.d/zz_automation.sh"
+    local sys_profile_path="${actual_inst_path}/$spp"
+    local inst_perm_arg="-o root -g root"
+    local am_root=0
+    if [[ $UID -eq 0 ]]; then
+        dbg "Will try to install and configure system-wide"
+        am_root=1
+        sys_profile_path="/$spp"
+    else
+       msg "Warning: Not installing as root, this is not recommended other than for testing purposes"
+       inst_perm_arg=""
+    fi
     # Allow re-installing different versions, clean out old version if found
     if [[ -d "$actual_inst_path" ]] && [[ -r "$actual_inst_path/AUTOMATION_VERSION" ]]; then
         local installed_version=$(cat "$actual_inst_path/AUTOMATION_VERSION")
         msg "Warning: Removing existing installed version '$installed_version'"
         rm -rvf "$actual_inst_path"
+        if ((am_root)); then
+            msg "Warning: Removing any existing, system-wide environment configuration"
+            rm -vf "/$spp"
+        fi
     elif [[ -d "$actual_inst_path" ]]; then
         msg "Error: Unable to deal with unknown contents of '$actual_inst_path', manual removal required"
-        msg "       Including any relevant lines in /etc/environment."
+        msg "       Including any relevant lines in /$spp"
         exit 12
     fi
 
-    if [[ "$DEFAULT_INSTALL_PREFIX" == "$INSTALL_PREFIX" ]]; then
-        # Do a system-wide install assuming permission check passes
-        local etc_env_filepath="/etc/environment"
-        local inst_perm_arg="-o root -g root"
-    elif [[ "$DEFAULT_INSTALL_PREFIX" != "$INSTALL_PREFIX" ]] || [[ "$UID" -ne 0 ]]; then
-        # running under unit-tests or some other abnormal environment
-        etc_env_filepath="$actual_inst_path/environment"
-        inst_perm_arg=""
-    fi
+    msg "Installing common scripts/libraries version '$AUTOMATION_VERSION' into '$actual_inst_path'"
 
     cd "$TEMPDIR/common"
     install -v $inst_perm_arg -D -t "$actual_inst_path/bin" ./bin/*
     install -v $inst_perm_arg -D -t "$actual_inst_path/lib" ./lib/*
-    cd "$TEMPDIR"
-    install -v $inst_perm_arg -D -t "$actual_inst_path/bin" "./bin/$SCRIPT_FILENAME"
 
-    msg "Configuring \$AUTOMATION_LIB_PATH=$actual_inst_path/lib in '$etc_env_filepath'"
-    echo "export AUTOMATION_LIB_PATH='$actual_inst_path/lib'" >> "$etc_env_filepath"
+    cd "$actual_inst_path"
+    dbg "Configuring example environment in $actual_inst_path/environment"
+    cat <<EOF>"./environment"
+# Added on $(date --iso-8601=minutes) by $actual_inst_path/bin/$SCRIPT_FILENAME"
+export AUTOMATION_LIB_PATH="$actual_inst_path/lib"
+export PATH="\${PATH:+\$PATH:}$actual_inst_path/bin"
+EOF
+    if ((am_root)); then
+        msg "Installing example environment files system-wide"
+        install -v $inst_perm_arg -D -t "$(dirname /$spp)" "./environment"
+    fi
 
-    # Last step marks a clean install
-    echo "$AUTOMATION_VERSION" >> "$actual_inst_path/AUTOMATION_VERSION"
+    echo -n "Installation complete for " > /dev/stderr
+    echo "$AUTOMATION_VERSION" | tee "./AUTOMATION_VERSION" > /dev/stderr
 }
 
 exec_installer() {
+    # Actual version string may differ from $AUTOMATION_VERSION argument
+    local version_arg
     if [[ -z "$TEMPDIR" ]] || [[ ! -d "$TEMPDIR" ]]; then
         msg "Error: exec_installer() expected $TEMPDIR to exist"
         exit 13
@@ -91,44 +115,49 @@ exec_installer() {
     # Special-case, use existing source repository
     if [[ "$AUTOMATION_VERSION" == "$MAGIC_LOCAL_VERSION" ]]; then
         cd $(realpath "$(dirname ${BASH_SOURCE[0]})/../")
+        dbg "Will try to use installer from local repository $PWD"
         # Make sure it really is a git repository
         if [[ ! -r "./.git/config" ]]; then
             msg "ErrorL Must execute $SCRIPT_FILENAME from a repository clone."
             exit 6
         fi
-        # Force version to be installed as the current local repository version
-        AUTOMATION_VERSION="0.0.0"
         # Allow installer to clean-up TEMPDIR as with updated source
+        dbg "Copying repository into \$TEMPDIR"
         cp --archive ./* ./.??* "$TEMPDIR/."
+        dbg "Now working from \$TEMPDIR"
+        cd "$TEMPDIR"
+        if [[ "$(git rev-parse --is-shallow-repository)" == "true" ]]; then
+            msg "Retrieving complete version information for local repository"
+            $OOE git fetch --unshallow --tags
+        fi
+        msg "Attempting to rettrieve actual version based on all configured remotes"
+        version_arg=$(git describe HEAD)
     else  # Retrieve the requested version (tag) of the source code
-        local version_arg="v$AUTOMATION_VERSION"
+        version_arg="v$AUTOMATION_VERSION"
         if [[ "$AUTOMATION_VERSION" == "latest" ]]; then
             version_arg=$AUTOMATION_REPO_BRANCH
         fi
         msg "Attempting to clone branch/tag '$version_arg'"
+        dbg "Cloning from $AUTOMATION_REPO_URL into \$TEMPDIR"
         git clone --quiet --branch "$version_arg" \
             --depth 1 --config advice.detachedHead=false \
             "$AUTOMATION_REPO_URL" "$TEMPDIR/."
-    fi
-
-    cd "$TEMPDIR"
-    if [[ "$AUTOMATION_VERSION" == "$MAGIC_LOCAL_VERSION" ]] || [[ "$AUTOMATION_VERSION" == "latest" ]]; then
-        # After install, precise version will be helpful to future sys admins
-        msg "Retrieving version information"
-        git fetch --tags
-        AUTOMATION_VERSION=$(git describe --always HEAD)
+        cd "$TEMPDIR"
     fi
 
     # Full path is required so script can find and install itself
     DOWNLOADED_INSTALLER="$TEMPDIR/bin/$SCRIPT_FILENAME"
     if [[ -x "$DOWNLOADED_INSTALLER" ]]; then
-        msg "Executing install for version '$AUTOMATION_VERSION'"
-        trap - EXIT  # Specific installer is now responsible for cleanup
+        msg "Executing install for actial version '$version_arg'"
+        dbg "Using \$INSTALL_PREFIX '$INSTALL_PREFIX'; installer $DOWNLOADED_INSTALLER"
         exec env \
+            DEBUG="$DEBUG" \
             TEMPDIR="$TEMPDIR" \
-            TEST_INSTALL_PREFIX="$TEST_INSTALL_PREFIX" \
+            INSTALL_PREFIX="$INSTALL_PREFIX" \
+            AUTOMATION_REPO_URL="$AUTOMATION_REPO_URL" \
+            AUTOMATION_REPO_BRANCH="$AUTOMATION_REPO_BRANCH" \
             _MAGIC_JUJU="$_DEFAULT_MAGIC_JUJU" \
-            /bin/bash "$DOWNLOADED_INSTALLER" "$AUTOMATION_VERSION"
+            /bin/bash "$DOWNLOADED_INSTALLER" "$version_arg"
     else
         msg "Error: '$DOWNLOADED_INSTALLER' does not exist or is not executable" > /dev/stderr
         # Allow exi
@@ -137,14 +166,18 @@ exec_installer() {
 }
 
 check_args() {
+    local arg_rx="^($AUTOMATION_REPO_BRANCH)|^(latest)|^(v?[0-9]+\.[0-9]+\.[0-9]+(-.+)?)"
+    dbg "Debugging enabled; Command-line was '$0${_ARGS:+ $_ARGS}'"
+    dbg "Argument validation regular-expresion '$arg_rx'"
     if [[ -z "$AUTOMATION_VERSION" ]]; then
         msg "Error: Must specify the version number to install, as the first and only argument."
         msg "       Use version '$MAGIC_LOCAL_VERSION' to install from local source."
         msg "       Use version 'latest' to install from current upstream"
         exit 2
-    elif ! echo "$AUTOMATION_VERSION" | egrep -q '^(latest)|^(v?[0-9]+\.[0-9]+\.[0-9]+(-.+)?)'; then
-        msg "Error: '$AUTOMATION_VERSION' does not appear to be a valid version number"
-        exit 4
+    elif ! echo "$AUTOMATION_VERSION" | \
+        egrep -q "$arg_rx"; then
+            msg "Error: '$AUTOMATION_VERSION' does not appear to be a valid version number"
+            exit 4
     fi
 }
 
@@ -154,10 +187,13 @@ check_args() {
 check_args
 
 if [[ "$_MAGIC_JUJU" == "XXXXX" ]]; then
+    dbg "Operating in source prep. mode"
     TEMPDIR=$(mktemp -p '' -d "tmp_${SCRIPT_FILENAME}_XXXXXXXX")
+    dbg "Using temporary directory '$TEMPDIR'"
     trap "rm -rf $TEMPDIR" EXIT  # version may be invalid or clone could fail or some other error
     exec_installer # Try to obtain version from source then run it
 elif [[ "$_MAGIC_JUJU" == "$_DEFAULT_MAGIC_JUJU" ]]; then
+    dbg "Operating in actual install mode (ID $_MAGIC_JUJU)"
     # Running from $TEMPDIR in requested version of source
     $_MAGIC_JUJU
 else # Something has gone horribly wrong
@@ -165,3 +201,5 @@ else # Something has gone horribly wrong
     msg "Please obtain and use a newer version of $SCRIPT_FILENAME which supports ID $_MAGIC_JUJU"
     exit 10
 fi
+
+dbg "Clean exit."
--- common/test/test-install_automation.sh
+++ common/test/test-install_automation.sh
@@ -3,14 +3,14 @@
 # Unit-tests for installation script with common scripts/libraries.
 # Also verifies test script is derived from library filename
 
-TEST_DIR=$(dirname ${BASH_SOURCE[0]})/../../bin/
+TEST_DIR=$(dirname ${BASH_SOURCE[0]})/../../bin
 source $(dirname ${BASH_SOURCE[0]})/testlib.sh || exit 1
 INSTALLER_FILEPATH="$TEST_DIR/$SUBJ_FILENAME"
-TEST_INSTALL_ROOT=$(mktemp -p '' -d tmp_${SUBJ_FILENAME}_XXXXXXXX)
+TEST_INSTALL_ROOT=$(mktemp -p '' -d "tmp_$(basename $0)_XXXXXXXX")
 trap "rm -rf $TEST_INSTALL_ROOT" EXIT
 
 # Receives special treatment in the installer script
-TEST_INSTALL_PREFIX="$TEST_INSTALL_ROOT/test/automation"
+export INSTALL_PREFIX="$TEST_INSTALL_ROOT/testing"
 
 test_cmd \
     "The installer exits non-zero with a helpful message when run without a version argument" \
@@ -34,28 +34,48 @@ test_cmd \
 
 test_cmd \
     "The installer successfully installs and configures \$TEST_INSTALL_ROOT" \
-    0 "Configuring.+$TEST_INSTALL_ROOT/automation/environment" \
-    env INSTALL_PREFIX="$TEST_INSTALL_ROOT" $INSTALLER_FILEPATH 0.0.0
+    0 "Installation complete" \
+    $INSTALLER_FILEPATH 0.0.0
 
 test_cmd \
     "The installer correctly removes/reinstalls \$TEST_INSTALL_ROOT" \
     0 "Warning: Removing existing installed version" \
-    env INSTALL_PREFIX="$TEST_INSTALL_ROOT" $INSTALLER_FILEPATH 0.0.0
+    $INSTALLER_FILEPATH 0.0.0
 
 test_cmd \
     "The re-installed version has AUTOMATION_VERSION file matching the current version" \
     0 "$(git describe HEAD)" \
-    cat "$TEST_INSTALL_ROOT/automation/AUTOMATION_VERSION"
+    cat "$INSTALL_PREFIX/automation/AUTOMATION_VERSION"
+
+load_example_environment() {
+    local _args="$@"
+    # Don't disturb testing
+    (
+        source "$INSTALL_PREFIX/automation/environment" || return 99
+        echo "AUTOMATION_LIB_PATH ==> ${AUTOMATION_LIB_PATH:-UNDEFINED}"
+        echo "PATH ==> ${PATH:-EMPTY}"
+        [[ -z "$_args" ]] || $_args
+    )
+}
+
+execute_in_example_environment() {
+    load_example_environment "$@"
+}
 
 test_cmd \
-    "The non-DEFAULT_INSTALL_ROOT configured environment file defines AUTOMATION_LIB_PATH" \
-    0 "^export AUTOMATION_LIB_PATH=" \
-    cat $TEST_INSTALL_ROOT/automation/environment
+    "The example environment defines AUTOMATION_LIB_PATH" \
+    0 "AUTOMATION_LIB_PATH ==> $INSTALL_PREFIX/automation/lib" \
+    load_example_environment
 
 test_cmd \
-    "The installer can install the latest upstream version" \
-    0 "Configuring.+$TEST_INSTALL_ROOT/automation/environment" \
-    env INSTALL_PREFIX="$TEST_INSTALL_ROOT" $INSTALLER_FILEPATH latest
+    "The example environment appends to \$PATH" \
+    0 "PATH ==> .+:$INSTALL_PREFIX/automation/bin" \
+    load_example_environment
+
+test_cmd \
+    "The installed installer, can update itself to the latest upstream version" \
+    0 "Installation complete" \
+    execute_in_example_environment $SUBJ_FILENAME latest
 
 # Must be last call
 exit_with_status
